94f061481c4d3e66f7ebbc5dccf4af4fa6e9b568
Shotaro Doi <startaiyo0104@gmail.com>
2021-03-07 22:37:44 +0900

    bootstrap applied

--- creaapp/forms.py
+++ creaapp/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class HowmanyInfo(forms.Form):
-  many=forms.IntegerField()
+  def __init__(self, *args, **kwargs):
+    super(HowmanyInfo, self).__init__(*args, **kwargs)
+    for field in self.fields.values():
+        field.widget.attrs["class"] = "form-control"
+  
+  many=forms.IntegerField(label='個数')
--- mailauto_project/settings.py
+++ mailauto_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from .security import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'creaapp',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
